7fb982173a0972d03d11ac199ea6605f61cf70ff
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-10-24 22:08:25 -0500

    Update indexcut2.js

--- indexcut2.js
+++ indexcut2.js
@@ -27,21 +27,7 @@ let mm = gsap.matchMedia();
 Match media
 */
 
-    const _myDiv_ = document.getElementById('flextexts2');
-  _myDiv_.addEventListener('click', function() {
-        // Code to execute when the div is clicked
-        console.log('Div with ID "myDivId" was clicked!');
-    });
-    const myDiv_ = document.getElementById('flextexts3');
-  myDiv_.addEventListener('click', function() {
-        // Code to execute when the div is clicked
-        console.log('Div with ID "myDivId" was clicked!');
-    });
-        const myDiv__ = document.getElementById('flextexts4');
-      myDiv__.addEventListener('click', function() {
-        // Code to execute when the div is clicked
-        console.log('Div with ID "myDivId" was clicked!');
-    });
+
     
 mm.add("(max-width: 767px)", () => {
     
@@ -180,7 +166,26 @@ if (distanceXend <= -calculateVwToPx(205))
       // Snap to the target if the threshold is met
 {
 
-turnRight3();
+gsap.to(".panel.three",{
+x:-calculateVwToPx(300),
+});
+
+gsap.to(".panel.four",{
+x:-calculateVwToPx(300),
+});
+
+    gsap.to(".flextexts3",{
+x:-calculateVwToPx(270),
+});
+gsap.to(".flextexts4",{
+x:-calculateVwToPx(270),
+});
+
+    
+console.log('snap');
+this.disable();
+draggable4.enable();
+     
 
 
 
@@ -270,7 +275,29 @@ if (distanceXend <= -calculateVwToPx(120))
 {
 
 
-turnRight2();
+gsap.to(".panel.two",{
+x:-calculateVwToPx(200),
+});
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(200),
+});
+
+gsap.to(".panel.four",{
+x:-calculateVwToPx(200),
+duration:0,
+});
+gsap.to(".flextexts2",{
+x:-calculateVwToPx(180),
+});
+gsap.to(".flextexts3",{
+x:-calculateVwToPx(180),
+});
+
+
+this.disable();
+draggable3.enable();     
+   
 
 }
 
@@ -323,29 +350,6 @@ if (distanceXend <= - calculateVwToPx(25) ) {
       // Snap to the target if the threshold is met
 
 
-turnRight1();
-
-
-    } else {
-      // Snap back to the original position if not
-    }
-    
-gsap.to(".panel.two",{
-x:this.x,
-duration:0,
-});
-
-},
- onDragEnd: function() {
-
-  }
-})[0];
-
-});
-
-function turnRight1()
-      {
-
 gsap.to(".panel.one",{
 x:-calculateVwToPx(100),
 });
@@ -368,11 +372,76 @@ x:-calculateVwToPx(90),
     
 draggable1.disable();
 draggable2.enable();
-         return; 
+         
+
+
+    } else {
+      // Snap back to the original position if not
+    }
+    
+gsap.to(".panel.two",{
+x:this.x,
+duration:0,
+});
+
+},
+ onDragEnd: function() {
+
+  }
+})[0];
+
+
+
+    const _myDiv_ = document.getElementById('flextexts2');
+  _myDiv_.addEventListener('click', function() {
+        gsap.to(".panel.one",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.two",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(100),
+duration:0,
+});
+
+gsap.to(".flextexts1",{
+x:-calculateVwToPx(90),
+});
+gsap.to(".flextexts2",{
+x:-calculateVwToPx(90),
+});
+    
+draggable1.disable();
+draggable2.enable();
+        console.log('Div with ID "myDivId" was clicked!');
+    });
+    const myDiv_ = document.getElementById('flextexts3');
+  myDiv_.addEventListener('click', function() {
+        // Code to execute when the div is clicked
+        console.log('Div with ID "myDivId" was clicked!');
+    });
+        const myDiv__ = document.getElementById('flextexts4');
+      myDiv__.addEventListener('click', function() {
+        // Code to execute when the div is clicked
+        console.log('Div with ID "myDivId" was clicked!');
+    });
+
+
+    
+});
+
+function turnRight1()
+      {
+
+
       }
   
 
     
+
 
 
 
