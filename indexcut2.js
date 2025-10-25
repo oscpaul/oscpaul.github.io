19a3cb3e046e61d17509f56a46b082bb622315a7
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-10-24 21:57:18 -0500

    Update indexcut2.js

--- indexcut2.js
+++ indexcut2.js
@@ -3,7 +3,97 @@
         return (viewportWidth * vwValue) / 100;
     }
 
+function turnRight1()
+      {
 
+gsap.to(".panel.one",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.two",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(100),
+duration:0,
+});
+
+gsap.to(".flextexts1",{
+x:-calculateVwToPx(90),
+});
+gsap.to(".flextexts2",{
+x:-calculateVwToPx(90),
+});
+    
+this.disable();
+draggable2.enable();
+         return; 
+      }
+
+
+
+
+
+
+    function turnRight2{
+
+gsap.to(".panel.two",{
+x:-calculateVwToPx(200),
+});
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(200),
+});
+
+gsap.to(".panel.four",{
+x:-calculateVwToPx(200),
+duration:0,
+});
+gsap.to(".flextexts2",{
+x:-calculateVwToPx(180),
+});
+gsap.to(".flextexts3",{
+x:-calculateVwToPx(180),
+});
+
+
+this.disable();
+draggable3.enable();     
+   
+         return; 
+
+ }
+
+   
+
+
+
+ function turnRight3(){
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(300),
+});
+
+gsap.to(".panel.four",{
+x:-calculateVwToPx(300),
+});
+
+    gsap.to(".flextexts3",{
+x:-calculateVwToPx(270),
+});
+gsap.to(".flextexts4",{
+x:-calculateVwToPx(270),
+});
+
+    
+console.log('snap');
+this.disable();
+draggable4.enable();
+     
+         return; 
+   
+    }
 
 
 
@@ -341,6 +431,7 @@ duration:0,
   
 
     
+
 
 
 
