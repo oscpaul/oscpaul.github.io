1ac41be803c47844c4582559e65fd702ab0f5e19
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-10-25 09:46:27 -0500

    Update indexcut2.js

--- indexcut2.js
+++ indexcut2.js
@@ -434,7 +434,20 @@ draggable2.enable();
     
 
 
+function lazyLoad(){
+const lazyImages=document.querySelectorAll('.lazy');
+lazyImages.forEach(img=>{
+if(img.getBoundingClientRect().left<window.innerWidth&&
+img.getBoundingClientRect().bottom>0)
+{
+img.src=img.dataset.src;
+img.classList.remove('lazy');
+}
+});
+}
 
+lazyLoad();
+window.addEventListener('scroll', lazyLoad);
 
     
 });
@@ -442,6 +455,7 @@ draggable2.enable();
 
 
     
+
 
 
 
