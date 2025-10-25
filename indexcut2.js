1d597ea6c3f41d187e93d9d57313b1eebf217d12
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-10-24 22:18:12 -0500

    Update indexcut2.js

--- indexcut2.js
+++ indexcut2.js
@@ -394,7 +394,28 @@ duration:0,
 
     const _myDiv_ = document.getElementById('flextexts2');
   _myDiv_.addEventListener('click', function() {
-      console.log(draggable1);
+      gsap.to(".panel.one",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.two",{
+x:-calculateVwToPx(100),
+});
+
+gsap.to(".panel.three",{
+x:-calculateVwToPx(100),
+duration:0,
+});
+
+gsap.to(".flextexts1",{
+x:-calculateVwToPx(90),
+});
+gsap.to(".flextexts2",{
+x:-calculateVwToPx(90),
+});
+    
+draggable1.disable();
+draggable2.enable();
         console.log('Div with ID "myDivId" was clicked!');
     });
     const myDiv_ = document.getElementById('flextexts3');
@@ -420,6 +441,7 @@ function turnRight1()
   
 
     
+
 
 
 
