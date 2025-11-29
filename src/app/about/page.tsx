982c010af8932c0c95759708c4638db6c843c7de
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-29 17:31:11 -0600

    Create page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import Gallery from '@/app/components/Home/Gallery'
+
+export default function Home() {
+  return (
+
+    <main>
+      
+    <div>
+           
+
+    </div>
+  
+     <Gallery />
+    
+    </main>
+  )
+}
