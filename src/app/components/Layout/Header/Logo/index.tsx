231a19a47e18fe5103ec48a8dbb301dfd68a7731
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-30 15:30:08 -0600

    Update index.tsx

--- src/app/components/Layout/Header/Logo/index.tsx
+++ src/app/components/Layout/Header/Logo/index.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
     import { usePathname } from 'next/navigation';
     import { userAgentFromString } from "next/server"; // For Next.js 13+ App Router
-      
+          import { headers } from 'next/headers';
+
 export function isServerMobile() {
   const headersList = headers();
   const userAgent = headersList.get("user-agent");
-    import { headers } from 'next/headers';
   const { device } = userAgentFromString(userAgent || undefined); // Use a library to parse for more detailed device info
   return device.type === "mobile";
 }
