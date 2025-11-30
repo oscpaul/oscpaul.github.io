a9c108dd4380801525fee402d4f39e43eed8726b
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-29 19:57:25 -0600

    Update index.tsx

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -10,8 +10,12 @@ import Signin from '@/app/components/Auth/SignIn'
 import SignUp from '@/app/components/Auth/SignUp'
 import { Icon } from '@iconify/react/dist/iconify.js'
 import { HeaderItem } from '@/app/types/menu'
+    import { usePathname } from 'next/navigation';
 
 const Header: React.FC = () => {
+
+      const pathname = usePathname();
+
   const [headerLink, setHeaderLink] = useState<HeaderItem[]>([])
 
   const [navbarOpen, setNavbarOpen] = useState(false)
@@ -23,6 +27,7 @@ const Header: React.FC = () => {
   const signInRef = useRef<HTMLDivElement>(null)
   const signUpRef = useRef<HTMLDivElement>(null)
   const mobileMenuRef = useRef<HTMLDivElement>(null)
+    
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,10 +43,14 @@ const Header: React.FC = () => {
     fetchData()
   }, [])
 
-  const handleScroll = () => {
-    setSticky(window.scrollY >= 20)
-  }
 
+
+  const handleScroll = () => {
+
+    setSticky(window.scrollY >= 20)
+     
+  }
+    
   const handleClickOutside = (event: MouseEvent) => {
     if (
       signInRef.current &&
@@ -65,11 +74,15 @@ const Header: React.FC = () => {
   }
 
   useEffect(() => {
+    
     window.addEventListener('scroll', handleScroll)
     document.addEventListener('mousedown', handleClickOutside)
     return () => {
+     
+     
       window.removeEventListener('scroll', handleScroll)
       document.removeEventListener('mousedown', handleClickOutside)
+          
     }
   }, [navbarOpen, isSignInOpen, isSignUpOpen])
 
@@ -80,12 +93,49 @@ const Header: React.FC = () => {
       document.body.style.overflow = ''
     }
   }, [isSignInOpen, isSignUpOpen, navbarOpen])
-
+  if (pathname === '/') {
+      console.log(pathname)
+  
   return (
     <header
       className={`fixed top-0 z-40  w-full transition-all duration-300 ${
         sticky ? ' shadow-lg bg-white' : 'shadow-none'
       }, ${sticky ? '  p-0' : 'height-auto p-4'}`}>
+      <div>
+        <div className='backgroundDiv container flex items-center justify-between' style={{ }}>
+          
+          <div>
+            <Logo />
+          </div>
+             <nav className="flex gap-6 text-lg font-medium">
+          <Link href="/" className="hover:text-gray-300 transition">
+            Home
+          </Link>
+
+          <Link href="/about" className="hover:text-gray-300 transition">
+            Menu
+          </Link>
+
+          <Link
+            href="/order"
+            className="bg-white text-blue-700 px-4 py-2 rounded-md font-semibold hover:bg-gray-100 transition"
+          >
+            Order Now
+          </Link>
+        </nav>
+  
+        </div>
+      
+  
+      </div>
+    </header>
+  )
+  }
+  else
+     return (
+    <header
+      className={`fixed top-0 z-40  w-full transition-all duration-300 
+       ${sticky ? 1 : 0}`}>
       <div>
         <div className='backgroundDiv container flex items-center justify-between' style={{ }}>
           
