51820ed720edb791af1d95eaff123e6013569d99
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-30 16:12:42 -0600

    Update index.tsx

--- src/app/components/Layout/Header/Logo/index.tsx
+++ src/app/components/Layout/Header/Logo/index.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link'
     import { usePathname } from 'next/navigation';
     import { userAgentFromString } from "next/server"; // For Next.js 13+ App Router
 
-if (window.innerWidth>1)
-console.log("desk")
 const Logo: React.FC = () => {
 
       const pathname = usePathname();
