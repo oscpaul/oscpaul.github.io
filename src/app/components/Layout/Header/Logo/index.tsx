172b9eb994581b9ffdcd333bfde6cc0befecc0e0
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-29 20:34:09 -0600

    Update index.tsx

--- src/app/components/Layout/Header/Logo/index.tsx
+++ src/app/components/Layout/Header/Logo/index.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
+    import { usePathname } from 'next/navigation';
+
 
 const Logo: React.FC = () => {
+
+      const pathname = usePathname();
+  if (pathname === '/') {
   return (
     <Link href='/' className='flex items-center gap-4'>
       <Image
@@ -12,9 +17,28 @@ const Logo: React.FC = () => {
         className='w-fit'
         quality={100}
       />
-      <p className='golden-text text-2xl font-semibold '>The Golden Lantern</p>
+      <p className='text-black text-2xl font-semibold '>The Golden Lantern</p>
     </Link>
   )
+  }
+
+  else
+    {
+  return (
+    <Link href='/' className='flex items-center gap-4'>
+      <Image
+        src='/images/Logo/Logo.svg'
+        alt='logo'
+        width={117}
+        height={34}
+        className='w-fit'
+        quality={100}
+      />
+      <p className='golden-text text-black text-2xl font-semibold '>The Golden Lantern</p>
+    </Link>
+  )
+  }
+
 }
 
 export default Logo
