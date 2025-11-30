5936e752b07c8e06cc15e0e7c9ea4557832e01b1
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-11-29 20:58:49 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,12 @@ export const metadata: Metadata = {
   title: 'Kitchen',
 }
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  // viewportFit: 'cover', // Optional, for full-screen experience on some devices
+};
 
 export default function Home() {
   return (
