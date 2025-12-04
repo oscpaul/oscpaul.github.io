5fed0263b9d6fc4121fadd470b120a8382595679
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-12-03 19:40:14 -0600

    Update index.tsx

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -112,7 +112,7 @@ const Header: React.FC = () => {
     <header
       className={`fixed top-10 z-40  w-full transition-all duration-300 ${
         sticky ? ' shadow-lg bg-white' : 'shadow-none'
-      }, ${sticky ? '  p-0' : 'p-4 p-0'}`}>
+      }, ${sticky ? 'height-auto  p-0' : 'height-auto p-4 p-0'}`}>
       <div>
 
         
