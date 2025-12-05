3716599c7acf461c04cd3f0376427f1ee00f33a2
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-12-04 23:53:22 -0600

    Update index.tsx

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -110,16 +110,16 @@ const Header: React.FC = () => {
   
   return (
 <header
-       className={`fixed top-0 z-40  w-full transition-all duration-300 ${
+       className={`fixed top0 z-40 flex w-full transition-all duration-300 ${
         sticky ? ' shadow-lg bg-white' : 'shadow-none'
-      }, ${sticky ? 'height-auto  p-0' : 'height-auto p-4 p-0'}`}>
+      }, ${sticky ? 'height-auto  p-0' : 'height-auto p-0'}`}>
       <div>
-        <div className='backgroundDiv container flex items-center justify-between' style={{ }}>
+        <div className='backgroundDiv container' style={{ }}>
           
           <div>
             <Logo />
           </div>
-             <nav className="header-nav flex gap-6 text-lg font-medium">
+             <nav className="header-nav flex text-lg font-medium">
           <Link href="/" className="hover:text-gray-300 transition">
             Home
           </Link>
@@ -130,7 +130,7 @@ const Header: React.FC = () => {
 
           <Link
             href="/order"
-            className="bg-white text-blue-700 px-4 py-2 rounded-md font-semibold hover:bg-gray-100 transition"
+            className="bg-white text-blue-700 px-4 rounded-md font-semibold hover:bg-gray-100 transition"
           >
             Order Now
           </Link>
