17b18046d5be5f52de75a68a20750e43ff545a66
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2025-12-04 23:49:33 -0600

    Update index.tsx

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -109,32 +109,31 @@ const Header: React.FC = () => {
       console.log(pathname)
   
   return (
-    <header
-       className={`fixed top-0 top0 z-40  w-full transition-all duration-300 ${
+<header
+       className={`fixed top-0 z-40  w-full transition-all duration-300 ${
         sticky ? ' shadow-lg bg-white' : 'shadow-none'
       }, ${sticky ? 'height-auto  p-0' : 'height-auto p-4 p-0'}`}>
-      <div className="flex1">
-
-        
-        <div className={`${myCustomFont.className} top0`} style={{ }}>
+      <div>
+        <div className='backgroundDiv container flex items-center justify-between' style={{ }}>
           
-          <div className="">
+          <div>
             <Logo />
           </div>
-             <nav className="flex3 w-full header-nav flex gap-6 text-lg font-medium top0">
-          <Link href="/" className="">
-           Home
+             <nav className="header-nav flex gap-6 text-lg font-medium">
+          <Link href="/" className="hover:text-gray-300 transition">
+            Home
           </Link>
 
-          <Link href="/about" className="">
-         Menu 
+          <Link href="/about" className="hover:text-gray-300 transition">
+            Menu
           </Link>
 
           <Link
             href="/order"
-            className=""
+            className="bg-white text-blue-700 px-4 py-2 rounded-md font-semibold hover:bg-gray-100 transition"
           >
-            Order Now        </Link>
+            Order Now
+          </Link>
         </nav>
   
         </div>
