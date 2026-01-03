4f87df1b7d91a5420cd1eca8e46de7de4b3e49e2
oscpaul <oscaipaul@gmail.com>
2026-01-02 21:59:09 -0600

    first commit

--- app/components/FullScreenVideo.tsx
+++ app/components/FullScreenVideo.tsx
@@ -36,11 +36,15 @@ return (
 
 <figure className="mx-auto w-auto">
 <div style={{aspectRatio:'1280 / 720'}} className="h-auto aspect-video w-full relative">
+   <div className="gutter-p absolute inset-0 z-md grid-layout py-6">
+            <div className="col-span-12 medium:col-span-6 medium:!col-start-7">
+                <h2 className="text-white sticky top-[68px] h1">Deploy anywhere</h2>
+            </div></div>
 <button className="absolute w-full h-full inset-0 group z-[1] flex items-center justify-center" aria-label="play video">
   </button>
   <div className="absolute inset-0 w-full h-full flex items-center justify-center">
 
-             <video
+           <video
         ref={videoRef}
         src={videos[currentVideoIndex]}
         autoPlay // Muted is often required for autoplay to work
