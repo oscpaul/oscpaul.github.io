8248491a470f4370f29461cdb291b80c6da7c93c
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2026-02-19 14:22:41 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -163,7 +163,7 @@ const schema = {
     "@type": "Organization",
     "name": "2 Brothers Auto & Muffler",
     "url": "https://devwordpressspace.site",
-    "logo": "https://devwordpressspace.site/images/services/logo.jpg"
+    
   }
 };
 
