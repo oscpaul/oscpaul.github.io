67fdaed0f0536547bcdd65accd781a9d0260393b
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2026-02-21 22:28:16 -0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,8 +18,10 @@ With decades of combined experience, our certified mechanics specialize in muffl
           </p>
 
           <div className="flex justify-center">
-            <button className="group relative overflow-hidden border border-charcoal px-2 py-2 rounded-full text-sm transition-all duration-300 hover:-translate-y-0.5">
-              {/* Highlight overlay */}
+              <a
+              href="#services"
+              className="group relative overflow-hidden border border-charcoal px-4 py-2 rounded-full text-sm transition-all duration-300 hover:-translate-y-0.5"
+            >
               <span
                 className="
                   absolute inset-0
@@ -30,7 +32,7 @@ With decades of combined experience, our certified mechanics specialize in muffl
                 "
               />
               <span className="relative z-10">Our Services</span>
-            </button>
+            </a>
           </div>
         </div>
       </section>
