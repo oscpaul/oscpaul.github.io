c709939a9fce0d77762a2300ae3109e542548807
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2026-02-23 21:59:31 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'devwordpressspace.site' }],
+        destination: 'https://devwordpressspace.site/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
