4acf9c8519719d9cdb0dc2d31e579e376f88d889
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2026-03-07 17:50:43 -0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,27 +24,23 @@ export default function Hero() {
   </div>
 
   {/* Social Icons */}
-  <ul className="flex justify-center gap-4 flex-wrap">
-    <li className="social__item">
-      <span className="visually-hidden">Instagram</span>
-      <span className="social__item--icon">{/* svg */}</span>
-    </li>
-
-    <li className="social__item">
-      <span className="visually-hidden">Twitter</span>
-      <span className="social__item--icon">{/* svg */}</span>
-    </li>
-
-    <li className="social__item">
-      <span className="visually-hidden">Github</span>
-      <span className="social__item--icon">{/* svg */}</span>
-    </li>
-
-    <li className="social__item">
-      <span className="visually-hidden">LinkedIn</span>
-      <span className="social__item--icon">{/* svg */}</span>
-    </li>
-  </ul>
+<ul className="social__items" role="list">
+  <li className="social__item">
+    <a href="https://www.instagram.com/_mariorhernandez/" title="Like me on Instagram" target="_blank">
+    <span className="visually-hidden">Instagram </span>
+    <span className="social__item--icon">
+      <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zM17.5 6.5h.01"></path>
+      </svg></span></a></li>
+      <li className="social__item">
+        <a href="https://twitter.com/imariohernandez" title="Follow me on Twitter" target="_blank"><span className="visually-hidden">Twitter </span>
+        <span className="social__item--icon">
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M21.3 21.1 9.9 2.9H2.7l11.4 18.2zM2.7 21.1l7.2-6.6M14.1 9.5l7.2-6.6" className="st0"></path></svg></span></a></li>
+      <li className="social__item">
+        <a href="https://github.com/mariohernandez" title="Collaborate with me on Github" target="_blank">
+        <span className="visually-hidden">Github </span>
+        <span className="social__item--icon"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"></path></svg></span></a></li>
+        <li className="social__item"><a href="https://www.linkedin.com/in/mariorhernandez/" title="Network with me on LinkedIn" target="_blank"><span class="visually-hidden">LinkedIn </span><span class="social__item--icon"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path><circle cx="4" cy="4" r="2"></circle></svg></span></a>
+        </li></ul>
 
   {/* CTA */}
   <div className="pt-2">
